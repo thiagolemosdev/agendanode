8efd17ff54de62296fab6675e9527f61af217011
Thiago Lemos <lemoos.th@gmail.com>
2022-08-26 05:39:17 +0000

    Editando contato

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -37,3 +37,27 @@ exports.editIndex = async function (req, res) {
     contato,
   });
 };
+
+exports.edit = async function (req, res) {
+  if (!req.params.id) return res.render("404");
+  try {
+    const contato = new Contato(req.body);
+
+    await contato.edit(req.params.id);
+
+    if (contato.errors.length > 0) {
+      req.flash("errors", contato.errors);
+      req.session.save(() => res.redirect("back"));
+      return;
+    }
+
+    req.flash("success", "Contato editado com sucesso");
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    );
+    return;
+  } catch (e) {
+    console.log(e);
+    return res.render("404");
+  }
+};
