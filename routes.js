f7ebb8b60ef37f1608ae72f2b7bbae08a8241ad5
Thiago Lemos <lemoos.th@gmail.com>
2022-08-26 04:47:54 +0000

    Criando Usuario

--- routes.js
+++ routes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const route = express.Router();
 const homeController = require("./src/controllers/homeController");
 const loginController = require("./src/controllers/loginController");
+const contatoController = require("./src/controllers/contatoController");
 const {
   checkCsrfError,
   middlewareGlobal,
+  loginRequired,
 } = require("./src/middlewares/middleware");
 
 //Rotas da home page
@@ -15,5 +17,10 @@ route.get("/login/index", loginController.index);
 route.post("/login/register", loginController.register);
 route.post("/login/login", loginController.login);
 route.get("/login/logout", loginController.logout);
+
+//rotas de contato
+route.get("/contato/index", loginRequired, contatoController.index);
+route.post("/contato/register", loginRequired, contatoController.register);
+route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
 
 module.exports = route;
---
+++ src/models/ContatoModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const ContatoSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: false, default: "" },
+  email: { type: String, required: false, default: "" },
+  tel: { type: String, required: false, default: "" },
+  criadoEm: { type: Date, dafaul: Date.now },
+});
+
+const ContatoModel = mongoose.model("Contato", ContatoSchema);
+
+function Contato(body) {
+  this.body = body;
+  this.errors = [];
+  this.contato = null;
+}
+
+Contato.buscarPorId = async function (id) {
+  if (typeof id !== "string") return;
+  const user = await ContatoModel.findById(id);
+  return user;
+};
+
+Contato.prototype.register = async function () {
+  this.valida();
+  if (this.errors.length > 0) return;
+
+  this.contato = await ContatoModel.create(this.body);
+};
+
+Contato.prototype.valida = function () {
+  this.cleanUp();
+
+  // validação do usuario
+  // o email precisa ser valido
+  if (this.body.email && !validator.isEmail(this.body.email)) {
+    this.errors.push("Email inválido");
+  }
+
+  if (!this.body.nome) this.errors.push("Nome é um campo obrigatorio");
+
+  if (!this.body.email && !this.body.tel) {
+    this.errors.push("Você precisa adicionar um email ou telefone");
+  }
+};
+
+Contato.prototype.cleanUp = function () {
+  for (const key in this.body) {
+    if (typeof this.body[key] !== "string") {
+      this.body[key] = "";
+    }
+  }
+  this.body = {
+    nome: this.body.nome,
+    sobrenome: this.body.sobrenome,
+    email: this.body.email,
+    tel: this.body.tel,
+  };
+};
+
+module.exports = Contato;
